27d854627abba820dd10eb17c1d4ff1d7a0df235
Kayhan.Kashi@gmail.com <Kayhan.Kashi@gmail.com>
2020-11-02 11:01:35 +0330

    Localization completed with path changed

--- TicTacToe.WebUI/Areas/Registration/Controllers/HomeController.cs
+++ TicTacToe.WebUI/Areas/Registration/Controllers/HomeController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TicTacToe.WebUI.Areas.Registration.Controllers
 {
     [Area("Registration")]
+    [assembly: ResourceLocation("~/Localization/Registration")]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            var culture = Request.HttpContext.Session.GetString("culture");
+            ViewBag.Language = culture;
             return View();
+        }
+
+        public IActionResult SetCulture(string culture)
+        {
+            Request.HttpContext.Session.SetString("culture", culture);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ TicTacToe.WebUI/Services/CultureProviderResolverService.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.WebUI.Services
+{
+    public class CultureProviderResolverService : RequestCultureProvider
+    {
+        private static readonly char[] _cookieSeparator = new[] { '|' };
+        private static readonly string _culturePrefix = "c=";
+        private static readonly string _uiCulturePrefix = "uic=";
+
+        public override async Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+        {
+            string culture;
+            if (GetCultureFromQueryString(httpContext, out culture))
+                return new ProviderCultureResult(culture, culture);
+            else if (GetCultureFromCookies(httpContext, out culture))
+                return new ProviderCultureResult(culture, culture);
+            else if (GetCultureFromSession(httpContext, out culture))
+                return new ProviderCultureResult(culture, culture);
+
+            return await NullProviderCultureResult;
+        }
+
+        private bool GetCultureFromQueryString(HttpContext httpContext, out string culture)
+        {
+            if (httpContext.Request.QueryString.HasValue)
+            {
+                culture = httpContext.Request.Query["culture"];
+                return true;
+            }
+            culture = null;
+            return false;
+        }
+
+        private bool GetCultureFromCookies(HttpContext httpContext, out string culture)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            var cookie = httpContext.Request.Cookies["culture"];
+            if (string.IsNullOrEmpty(cookie))
+            {
+                culture = null;
+                return false;
+            }
+            else
+            {
+                culture = ParseCookieValue(cookie);
+                return !string.IsNullOrEmpty(culture);
+            }
+        }
+
+        private bool GetCultureFromSession(HttpContext httpContext, out string culture)
+        {
+            culture = httpContext.Session.GetString("culture");
+            return !string.IsNullOrEmpty(culture);
+        }
+
+        public static string ParseCookieValue(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            var parts = value.Split(_cookieSeparator,
+             StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+            {
+                return null;
+            }
+
+            var potentialCultureName = parts[0];
+            var potentialUICultureName = parts[1];
+
+            if (!potentialCultureName.StartsWith(_culturePrefix) || !potentialUICultureName.StartsWith(_uiCulturePrefix))
+            {
+                return null;
+            }
+
+            var cultureName = potentialCultureName.Substring(_culturePrefix.Length);
+            var uiCultureName = potentialUICultureName.Substring(_uiCulturePrefix.Length);
+
+            if (cultureName == null && uiCultureName == null)
+            {
+                return null;
+            }
+
+            if (cultureName != null && uiCultureName == null)
+            {
+                uiCultureName = cultureName;
+            }
+
+            if (cultureName == null && uiCultureName != null)
+            {
+                cultureName = uiCultureName;
+            }
+
+            return cultureName;
+        }
+
+
+    }
+}
